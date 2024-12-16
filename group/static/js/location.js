fa4478ac97bd53192cdd2105a7a3144527237217
Ming_Official <61348264+mamama-wja@users.noreply.github.com>
2024-12-16 21:31:16 +0800

    Add files via upload

---
+++ group/static/js/location.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var domain = "xxxx.com";
+	$("#k30s").click(function(){
+        window.open("https://qm.qq.com/cgi-bin/qm/qr?k=HiPjhr3IhfnJTNJwEyarXG3m7WN4zbDm&jump_from=webapi&authKey=SbLmKQGiNN45utg98WuteMcuzVSwIG+k2+cpSPxuItSIzoV13bM6yvoqWtEttQJ+");
+    });
+    $("#12s").click(function(){
+        window.open("https://qm.qq.com/cgi-bin/qm/qr?k=qCWMCEiBelLS8Am4GrpynXbmPDRHX6lC&jump_from=webapi&authKey=11Am1njwF6i+95mClkxMpXzQxTc2bpvCA9FwRWUQy5weviHQeDF5/kkoTPVqV9yE");
+    });
+})
